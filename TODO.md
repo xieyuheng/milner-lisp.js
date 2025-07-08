5263cf6ee3f7a2183ac47f48c6550fc48d6a68b1
Xie Yuheng <xyheme@gmail.com>
2025-07-09 05:14:31 +0800

    [type] infer/infer

--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
-[type] infer/infer
+[type] infer/infer -- Apply
+[type] infer/infer -- Lambda
+[type] infer/infer -- Let
+`typeVarGen`
+`typeSchemeGen`
 [type] run -- call infer
 [type] parse type -- support `the`
--- src/lang/infer/infer.ts
+++ src/lang/infer/infer.ts
@@ -1,10 +1,35 @@
-import type { Ctx } from "../ctx/index.ts"
+import { ctxFind, type Ctx } from "../ctx/index.ts"
 import type { Exp } from "../exp/index.ts"
-import type { Subst } from "../subst/index.ts"
-import type { Type } from "../type/index.ts"
+import { substEmpty, type Subst } from "../subst/index.ts"
+import type { Type, TypeScheme } from "../type/index.ts"
 
 export type Inferred = { subst: Subst; type: Type }
 
-export function infer(ctx: Ctx, exp: Exp) {
+export function typeSchemeGen(typeScheme: TypeScheme): Type {
   throw new Error()
+}
+
+export function infer(ctx: Ctx, exp: Exp): Inferred {
+  switch (exp.kind) {
+    case "Var": {
+      const typeScheme = ctxFind(ctx, exp.name)
+      if (!typeScheme) throw new Error(`[infer] undefined name: ${exp.name}`)
+      return {
+        subst: substEmpty(),
+        type: typeSchemeGen(typeScheme),
+      }
+    }
+
+    case "Lambda": {
+      throw new Error()
+    }
+
+    case "Apply": {
+      throw new Error()
+    }
+
+    case "Let": {
+      throw new Error()
+    }
+  }
 }
